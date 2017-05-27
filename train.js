0435128a44ae39811af547e675fbe1ce47181b2b
leenbean16 <shamoonlina@gmail.com>
2017-05-26 22:04:40 -0700

    table works

--- train.js
+++ train.js
@@ -20,23 +20,22 @@ $("#submitButton").on("click", function(event) {
     event.preventDefault();
     var trainName = $("#trainname").val().trim().toLowerCase();
     var destination = $("#destination").val().trim();
-    var firstTrain = $("#firstTrain").val().trim();
     var frequency = $("#inputFrequency").val();
+    var firstTrain = $("#firstTrain").val();
     var exists = true;
     $("#trainname").val("")
     $("#destination").val("")
-    $("#firstTrain").val("")
     $("#inputFrequency").val("")
+    $("#firstTrain").val("")
 
     var newTrain = {
         trainName: trainName,
         destination: destination,
-        firstTrain: firstTrain,
         frequency: frequency,
+        firstTrain: firstTrain,
     }
-    console.log(newTrain);
 
-    train.child('trains').set(newTrain);
+    train.push().set(newTrain);
 })
 
 train.on("child_added", function(snapshot) {
@@ -44,25 +43,18 @@ train.on("child_added", function(snapshot) {
     var destination = snapshot.val().destination;
     var firstTrain = snapshot.val().firstTrain;
     var frequency = snapshot.val().frequency
-    console.log(trainName, destination, firstTrain, frequency);
+    var row = $('<tr>');
+    var dataarea = $('<td>').text(trainName);
+    var dataareab = $('<td>').text(destination);
+    var dataaread = $('<td>').text(frequency);
+    var dataareac = $('<td>').text(firstTrain);
+    var body = $('tbody');
 
+    console.log("CHILD ADDED:", snapshot.val().trainName, destination, firstTrain, frequency);
 
-$("#submitButton").on("click", function htmlshit() {
-    var dataarea = $('id', 'tn');
-    var dataareab = $('id', 'd');
-    var dataareac = $('id', 'ft');
-    var dataaread = $('id', 'na');
-    var dataareae = $('id', 'ma');
-    dataarea.attr('<td>');
-    dataareab.attr('<td>');
-    dataareac.attr('<td>');
-    dataaread.attr('<td>');
-    dataareae.attr('<td>');
-    $('#tn').val(trainName);
-    $('#d').val(destination);
-    $('#ft').val(firstTrain);
-    $('#na').val(frequency);
-    console.log(htmlshit);
-
-  })
+    row.append(dataarea);
+    row.append(dataareab);
+    row.append(dataaread);
+    row.append(dataareac);
+    body.append(row);
 });
